be5257052ef82167a8e1a7151af4a631704e2493
Toni Spets <toni.spets@iki.fi>
2021-05-03 21:50:11 +0300

    Improve IRC flood prevention
    QuakeNet has a byte based flood detection and these length delays
    are now implemented in a highly scientific way of testing on the
    network until it didn't flood out anymore.
    
    It still lets through normal chat by the user and should only
    affect pasting a lot of text.

--- heisenbridge/irc.py
+++ heisenbridge/irc.py
@@ -10,6 +10,9 @@ class HeisenConnection(AioConnection):
         self._queue = asyncio.Queue()
         self._task = asyncio.ensure_future(self._run())
 
+    def __del__(self):
+        self._task.cancel()
+
     async def _run(self):
         loop = asyncio.get_event_loop()
         last = loop.time()
@@ -29,17 +32,22 @@ class HeisenConnection(AioConnection):
                     if penalty < 0:
                         penalty = 0
 
-                if penalty > 5:
-                    await asyncio.sleep(1.5)
-
                 super().send_raw(string)
+
+                # sleep is based on message length
+                sleep_time = max(len(string.encode()) / 512 * 6, 1.5)
+
+                if penalty > 5 or sleep_time > 1.5:
+                    await asyncio.sleep(sleep_time)
 
                 # this needs to be reset if we slept
                 last = loop.time()
             except asyncio.CancelledError:
                 return
-            finally:
-                self._queue.task_done()
+            except Exception:
+                logging.exception("Failed to flush IRC queue")
+
+            self._queue.task_done()
 
     def send_raw(self, string):
         self._queue.put_nowait(string)

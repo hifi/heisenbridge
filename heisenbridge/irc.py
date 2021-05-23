6e2842817fe94647936b7ab1f9f9f94d95349b1c
Toni Spets <toni.spets@iki.fi>
2021-05-23 14:48:17 +0300

    Dead connection watchdog
    Should help with automatic disconnection from servers that have
    gone under.

--- heisenbridge/irc.py
+++ heisenbridge/irc.py
@@ -3,9 +3,50 @@ import logging
 
 from irc.client_aio import AioConnection
 from irc.client_aio import AioReactor
+from irc.client_aio import IrcProtocol
+
+
+class HeisenProtocol(IrcProtocol):
+    ping_timeout = 300
+
+    def connection_made(self, transport):
+        super().connection_made(transport)
+
+        # start aliveness check
+        self.loop.call_later(60, self._are_we_still_alive)
+        self.last_data = self.loop.time()
+
+    def data_received(self, data):
+        super().data_received(data)
+        self.last_data = self.loop.time()
+
+    def _are_we_still_alive(self):
+        # cancel if we don't have a connection
+        if not self.connection or not hasattr(self.connection, "connected") or not self.connection.connected:
+            logging.debug("Aliveness check has no connection, aborting.")
+            return
+
+        # no
+        if self.loop.time() - self.last_data >= self.ping_timeout:
+            logging.debug("Disconnecting due to no data received from server.")
+            self.connection.disconnect("No data received.")
+            return
+
+        # re-schedule aliveness check
+        self.loop.call_later(self.ping_timeout / 3, self._are_we_still_alive)
+
+        # yes
+        if self.loop.time() - self.last_data < self.ping_timeout / 3:
+            return
+
+        # perhaps, ask the server
+        logging.debug("Aliveness check failed, sending PING")
+        self.connection.send_raw("PING " + self.connection.real_server_name)
 
 
 class HeisenConnection(AioConnection):
+    protocol_class = HeisenProtocol
+
     def __init__(self, reactor):
         super().__init__(reactor)
         self._queue = asyncio.Queue()
@@ -65,7 +106,7 @@ class HeisenReactor(AioReactor):
         with self.mutex:
             matching_handlers = sorted(self.handlers.get("all_events", []) + self.handlers.get(event.type, []))
 
-            if len(matching_handlers) == 0 and event.type != "all_raw_messages":
+            if len(matching_handlers) == 0 and event.type != "all_raw_messages" and event.type != "pong":
                 matching_handlers += self.handlers.get("unhandled_events", [])
 
             for handler in matching_handlers:

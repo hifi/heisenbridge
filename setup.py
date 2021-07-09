e14411593aa0711f4cfd1bc5e01896467be849e0
Toni Spets <toni.spets@iki.fi>
2021-07-09 10:36:16 +0300

    Make Docker builds pick the version

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # Included to allow for editable installs
 import importlib.util
 
+from setuptools import Command
 from setuptools import setup
 from setuptools.command.build_py import build_py
 
@@ -10,18 +11,30 @@ version = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(version)
 
 
-class BuildPyCommand(build_py):
-    def run(self):
+class GenerateVersionCommand(Command):
+    description = "Generate version.txt"
+    user_options = []
 
+    def run(self):
         with open("heisenbridge/version.txt", "w") as version_file:
             version_file.write(version.__version__)
 
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+class BuildPyCommand(build_py):
+    def run(self):
+        GenerateVersionCommand.run(None)
         build_py.run(self)
 
 
 setup(
     version=version.__version__,
-    cmdclass={"build_py": BuildPyCommand},
+    cmdclass={"gen_version": GenerateVersionCommand, "build_py": BuildPyCommand},
     packages=["heisenbridge"],
     package_data={"heisenbridge": ["version.txt"]},
 )
